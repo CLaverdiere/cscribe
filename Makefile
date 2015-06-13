bb3d9530395e0a7def7a4066eeb57d73babbc454
Chris Laverdiere <cmlaverdiere@gmail.com>
2015-06-13 18:30:07 -0400

    Use portaudio and libsndfile for audio support.

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CC=gcc
-LIBS=-lncurses
+LIBS=-lncurses -lsndfile -lportaudio -pthread
 FLAGS=-Wall -g
 
 cscribe: cscribe.c
--- cscribe.c
+++ cscribe.c
@@ -1,22 +1,29 @@
-// TODO find library that can increase tempo, is lightweight and fast.
+// cscribe.c - Chris Laverdiere 2015
 
 #define _GNU_SOURCE
+#define SAMPLE_RATE 44100
 
 #include <libgen.h>
-#include <ncurses.h>
+#include <pthread.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/param.h>
+#include <unistd.h>
+
+#include <ncurses.h>
+#include <portaudio.h>
+#include <sndfile.h>
 
 #define TEMPO_DELTA .05
 #define TIME_DELTA 2
 
-static int max_col, max_row;
-static int quit;
-static char* mode_line;
-static FILE* audio_f;
+struct pa_data {
+  SNDFILE *sndfile;
+  SF_INFO sf_info;
+  int pos;
+} audio_data;
 
 struct progress_bar {
   int row, col;
@@ -32,13 +39,71 @@ struct song {
   char* name;
 } current_song;
 
+
+void cleanup();
+
+void* init_audio();
+void init_curses();
+
 void seek_seconds(int);
 void set_mark(int);
 void set_tempo(float);
+
 void show_help();
-void show_main();
+void show_greeting();
+void* show_main();
+void show_modeline();
 void show_progress_bar();
 void show_song_info();
+
+static int max_col, max_row;
+static int quit, pa_on, curses_on;
+static int redraw_flag;
+static char* mode_line;
+static PaError err;
+static PaStream *stream;
+static char* welcome_msg = "Welcome to cscribe!\n\n";
+
+static int pa_callback(const void *input_buf,
+                       void *output_buf,
+                       unsigned long frame_cnt,
+                       const PaStreamCallbackTimeInfo* time_info,
+                       PaStreamCallbackFlags flags,
+                       void *data) {
+
+  struct pa_data* cb_data = (struct pa_data*) data;
+  int* out = (int *) output_buf;
+  int* cursor = out;
+  int size = frame_cnt;
+  int read;
+  int frames_left;
+
+  while (size > 0) {
+    sf_seek(cb_data->sndfile, cb_data->pos, SEEK_SET);
+    frames_left = cb_data->sf_info.frames - cb_data->pos;
+
+    if (size > frames_left) {
+      read = frames_left;
+      cb_data->pos = 0;
+    } else {
+      read = size;
+      cb_data->pos += read;
+    }
+
+    sf_readf_int(cb_data->sndfile, cursor, read);
+    cursor += read;
+    size -= read;
+  }
+
+  return paContinue;
+}
+
+void pa_error(PaError err) {
+  if (err != paNoError) {
+    fprintf(stderr, "PortAudio error: %s\n", Pa_GetErrorText(err));
+    exit(1);
+  }
+}
 
 // Prints text centered horizontally.
 void printw_center_x(int row, int max_col, char* fmt, ...) {
@@ -97,22 +162,7 @@ void show_help() {
   show_main();
 }
 
-// Displays the main screen.
-void show_main() {
-  int ch;
-  int mid_row, mid_col;
-  char* welcome_msg = "Welcome to cscribe!\n\n";
-
-  mid_row = max_row / 2;
-  mid_col = max_col / 2;
-
-  clear();
-
-  // TODO we should use a separate window for the modeline.
-  if (mode_line == NULL) {
-    mode_line = "Type h for the list of all commands.";
-  }
-
+void show_greeting() {
   printw_center_x(1, max_col, welcome_msg);
 
   if (current_song.name == NULL) {
@@ -123,15 +173,37 @@ void show_main() {
     show_song_info();
   }
 
-  pbar.col = mid_col / 2;
-  pbar.row = mid_row;
-  pbar.len = mid_col;
+}
+
+void show_modeline() {
+  mvprintw(max_row - 1, 0, mode_line);
+}
+
+// Displays the main screen.
+void* show_main(void* args) {
+  int ch;
+
+  if (!curses_on) {
+    init_curses();
+    usleep(50000);
+  }
+
+  // TODO We should use a separate window for the modeline.
+  if (mode_line == NULL) {
+    mode_line = "Type h for the list of all commands.";
+  }
+
+  clear();
+  show_greeting();
+  show_modeline();
   show_progress_bar();
 
-  mvprintw(max_row - 1, 0, mode_line);
-  refresh();
-
   while (!quit && (ch = getch())) {
+    if (redraw_flag) {
+      redraw_flag = 0;
+      show_main(NULL);
+    }
+
     switch(ch) {
       case '\'':
         seek_seconds(current_song.mark);
@@ -144,7 +216,7 @@ void show_main() {
         break;
       case 'q':
         quit = 1;
-        return;
+        return NULL;
       case 'j':
         seek_seconds(current_song.time - TIME_DELTA);
         break;
@@ -159,6 +231,8 @@ void show_main() {
         break;
     }
   }
+
+  return NULL;
 }
 
 // Places a progress bar starting at row and col.
@@ -166,6 +240,10 @@ void show_main() {
 void show_progress_bar() {
   int pos = 0;
   int mark_pos = (float) current_song.mark / current_song.len * pbar.len;
+
+  pbar.col = max_col / 4;
+  pbar.row = max_row / 2;
+  pbar.len = max_col / 2;
 
   mvaddch(pbar.row, pbar.col, '[');
 
@@ -197,38 +275,80 @@ void show_song_info() {
   }
 }
 
+void* init_audio(void* args) {
+  PaStreamParameters out_params;
+
+  audio_data.pos = 0;
+  audio_data.sf_info.format = 0;
+  audio_data.sndfile = sf_open(current_song.name, SFM_READ, &audio_data.sf_info);
+
+  if (!audio_data.sndfile) {
+    fprintf(stderr, "Couldn't open %s\n", current_song.name);
+  }
+
+  Pa_Initialize();
+  pa_on = 1;
+
+  out_params.device = Pa_GetDefaultOutputDevice();
+  out_params.channelCount = audio_data.sf_info.channels;
+  out_params.suggestedLatency = 0.2;
+  out_params.sampleFormat = paInt32;
+  out_params.hostApiSpecificStreamInfo = 0;
+
+  Pa_OpenStream(&stream, 0, &out_params, audio_data.sf_info.samplerate,
+                      paFramesPerBufferUnspecified, paNoFlag, pa_callback,
+                      &audio_data);
+
+  Pa_StartStream(stream);
+
+  while (Pa_IsStreamActive(stream) == 1 && !quit) {
+    Pa_Sleep(100);
+  }
+
+  Pa_StopStream(stream);
+
+  return NULL;
+}
+
+void init_curses() {
+  initscr();
+  cbreak();
+  noecho();
+  curs_set(0);
+  keypad(stdscr, TRUE);
+  getmaxyx(stdscr, max_row, max_col);
+
+  curses_on = 1;
+}
+
+void cleanup() {
+  if (pa_on) Pa_Terminate();
+  if (curses_on) endwin();
+}
 
 int main(int argc, char* argv[])
 {
+  pthread_t curses_thread, audio_thread;
+
+  atexit(cleanup);
+
   if (argc > 2) {
     fprintf(stderr, "cscribe <audio_file>\n");
   }
 
   if (argc == 2) {
     char* audio_name = strdup(argv[1]);
-    audio_f = fopen(audio_name, "r");
 
     current_song.name = audio_name;
     current_song.len = 200; // TODO placeholder
     current_song.tempo = 1.0;
-
-    if (audio_f == NULL) {
-      fprintf(stderr, "File %s doesn't exist. Exiting cscribe.\n", audio_name);
-      goto cleanup;
-    }
   }
 
-  initscr();
-  raw();
-  noecho();
-  curs_set(0);
-  keypad(stdscr, TRUE);
-  getmaxyx(stdscr, max_row, max_col);
+  pthread_create(&audio_thread, NULL, init_audio, NULL);
+  pthread_create(&curses_thread, NULL, show_main, NULL);
 
-  show_main(audio_f);
-
-cleanup:
-  endwin();
+  pthread_join(curses_thread, NULL);
+  pthread_join(audio_thread, NULL);
 
   return 0;
 }
